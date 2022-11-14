da7b72a9cab66a99c3119f9020877f7e8cef367c
Fidel <freyesllerena33@gmail.com>
2022-11-14 18:05:54 +0100

    Look mom! A real app

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
